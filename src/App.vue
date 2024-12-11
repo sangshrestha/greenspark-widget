843625ff3f56c63d94c15a0a5ee9ad9d4e6672fc
sangshrestha <sang.socials@gmail.com>
2024-12-11 21:30:47 +0000

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h1>Per product widgets</h1>
+  </header>
+
+  <main>Main</main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/ProductWidget.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="product-widget"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
