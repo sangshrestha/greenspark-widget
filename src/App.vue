3200224dcc998eea46bea1e016d74386befc4bc0
sangshrestha <sang.socials@gmail.com>
2024-12-12 20:24:18 +0000

    feat: load widget with colour schemes

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,63 @@
     <h1>Per product widgets</h1>
   </header>
 
-  <main>Main</main>
+  <main>
+    <p v-if="widgetIsLoading">Loading widget...</p>
+    <template v-else>
+      <ProductWidget
+        v-for="widget in widgets"
+        :action="widget.action"
+        :amount="widget.amount"
+        :type="widget.type"
+        :selected-color="widget.selectedColor"
+      />
+    </template>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+import ProductWidget from "./components/ProductWidget.vue";
+
+interface WidgetProperty {
+  id: number;
+  type: string;
+  amount: number;
+  action: string;
+  active: boolean;
+  linked: boolean;
+  selectedColor: string;
+}
+
+const widgetIsLoading = ref(true);
+const widgets: Ref<WidgetProperty[]> = ref([]);
+
+async function fetchWidgets() {
+  try {
+    const response = await fetch(
+      "https://b795b019-1f84-41f4-93a3-a702d686c75a.mock.pstmn.io/product-widgets"
+    );
+    const widgets = await response.json();
+    return widgets;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchWidgets().then((e) => {
+  widgetIsLoading.value = false;
+  widgets.value = e;
+});
+</script>
 
 <style lang="scss" scoped>
 header {
   padding-bottom: 12px;
   border-bottom: 2px solid #b0b0b0;
+}
+
+main {
+  display: flex;
+  gap: 58px;
 }
 </style>
--- src/components/ProductWidget.vue
+++ src/components/ProductWidget.vue
@@ -1,7 +1,102 @@
 <template>
-  <div class="product-widget"></div>
+  <div class="widget" :class="[selectedColor, contrast]">
+    <div class="widget__header">
+      <div class="widget__logo">
+        <GreensparkLogo :dark="contrast === 'light'" />
+      </div>
+      <div class="widget__heading">
+        <p class="widget__heading-top">This product {{ action }}</p>
+        <p class="widget__heading-bottom">{{ amount }} {{ type }}</p>
+      </div>
+    </div>
+    <div class="widget__option">
+      <p class="widget__option-text">Link to Public Profile</p>
+    </div>
+    <div class="widget__option">
+      <p class="widget__option-text">Badge colour</p>
+    </div>
+    <div class="widget__option">
+      <p class="widget__option-text">Activate badge</p>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GreensparkLogo from "./GreensparkLogo.vue";
 
-<style lang="scss" scoped></style>
+const props = defineProps({
+  action: { type: String, required: true },
+  amount: { type: Number, required: true },
+  type: { type: String, required: true },
+  selectedColor: { type: String, required: true },
+});
+
+const lightColors = ["white", "beige"];
+
+const contrast = lightColors.includes(props.selectedColor) ? "light" : "dark";
+</script>
+
+<style lang="scss" scoped>
+.widget {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 222px;
+
+  &__icon {
+    width: fit-content;
+    height: fit-content;
+  }
+
+  &__header {
+    display: flex;
+    align-items: center;
+    height: 66px;
+    gap: 14px;
+    padding: 8px;
+
+    border-radius: 6px;
+  }
+
+  &__heading {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+
+    color: #fff;
+    font-size: 12.41px;
+
+    &-bottom {
+      font-size: 17.86px;
+      font-weight: 700;
+    }
+  }
+
+  &__option-text {
+    color: #3b755f;
+    font-size: 14px;
+  }
+
+  $colours: (
+    "blue": #2e3a8c,
+    "white": #fff,
+    "black": #212121,
+    "beige": #f2ebdb,
+    "green": #3b755f,
+  );
+
+  &.light {
+    .widget__heading {
+      color: #3b755f;
+    }
+  }
+
+  @each $colour, $hex in $colours {
+    &.#{$colour} {
+      .widget__header {
+        background-color: #{$hex};
+      }
+    }
+  }
+}
+</style>
